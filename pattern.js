9092e9a3d6bead689e350512de9d7ecf3b8b0d38
hsiat057 <hsiat057@newschool.edu>
2015-11-03 22:17:23 +0000

    msg ...

---
+++ pattern.js
@@ -0,0 +1,12 @@
+console.log("hashtag pattern"); 
+
+for (var multiplier = 1; multiplier < 11; multiplier++) { 
+console.log (multiplier*8); 
+
+var hashtag = "###"; 
+
+for (var i = 0; i < 10; i++) { 
+hashtag = hashtag + "#"; 
+console.log(hashtag); 
+     } 
+}
