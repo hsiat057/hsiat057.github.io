0fc6b0ce15eea42b3390484bc1ad70996a15e559
hsiat057 <hsiat057@newschool.edu>
2015-11-13 21:40:33 +0000

    msg ...

---
+++ Image Generator/generator.js
@@ -0,0 +1,26 @@
+for (var increment = 0; increment < 50; increment++) {
+  $("body").append('<div class="square"> </div>');
+}
+
+
+
+$(".square").mousemove(function() {
+    $(".square").each(function() {
+        var number = Math.floor(Math.random()*20);
+        console.log(number);
+
+        $(this).html("<img class = 'tile' src='bird-" + number + ".jpg'>");
+
+    });
+
+});
+
+$(window).click(function() {
+    $(".bird").each(function() {
+        var leftPosition = Math.floor(Math.random()*($(window).height()-60)) + "px";
+        var topPosition = Math.floor(Math.random()*($(window).width()-50)) + "px";
+        var topPosition = Math.floor(Math.random()*($(window).width()-50)) + "px";
+        $(this).css("left",leftPosition).css("top",topPosition);
+    });
+});
+
