0d28fce08cf44269c372ba71789ce104e01eaf5e
hsiat057 <hsiat057@newschool.edu>
2015-11-20 18:38:02 +0000

    msg ...

--- Image Generator/generator.js
+++ Image Generator/generator.js
@@ -1,29 +1,27 @@
 for (var increment = 0; increment < 50; increment++) {
-  $("body").append('<div class="square"> </div>');
+  $("body").append('<div class="moons"> </div>');
 }
 
-
-
-$(".square").mousemove(function() {
-    $(".square").each(function() {
-        var number = Math.floor(Math.random()*20);
-        console.log(number);
-        $(this).html("<img class = 'tile' src='bird-" + number + ".jpg'>");
-
-    });
-
+$(".moons").mouseover(function(){
+    $(".moons").css("background-color", "slategray");
+        var topPosition = Math.floor(Math.random()*($(window).height()-100)) + "px";
+        var leftPosition = Math.floor(Math.random()*($(window).width()-100)) + "px";
+        $(this).css("left",leftPosition);
+        var newColor = '#' + Math.floor(Math.random()*6902037595864385).toString(20);
+  $(this).css("border-color",newColor);
 });
 
+
 $(window).click(function() {
-    $(".bird").each(function() {
+    $(".moons").each(function() {
         var leftPosition = Math.floor(Math.random()*($(window).height()-60)) + "px";
         var topPosition = Math.floor(Math.random()*($(window).width()-50)) + "px";
         var topPosition = Math.floor(Math.random()*($(window).width()-50)) + "px";
         $(this).css("left",leftPosition).css("top",topPosition);
     });
 });
-    var logoHeight = $('bird').height();
+    var logoHeight = $('moons').height();
     if (logoHeight < 104) {
         var margintop = (104 - logoHeight) / 2;
-        $('bird').css('margin-top', margintop);
+        $('moons').css('margin-top', margintop);
     }
